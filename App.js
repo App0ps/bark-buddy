98e996d023d7e6fd92063d0ccd65b19264e0c3ea
Niles Thompson <dewand.th@gmail.com>
2024-03-22 15:58:14 -0700

    expo test

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './LoginScreen';
+import { Auth0ProviderWrapper } from './auth0Config'; // Import Auth0ProviderWrapper
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Auth0ProviderWrapper> {/* Wrap th NavigationContainer with Auth0ProviderWrapper */}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ title: 'Login' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Auth0ProviderWrapper>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
